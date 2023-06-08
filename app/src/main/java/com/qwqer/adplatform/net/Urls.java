618571a6b9fea55dd099121d3092cd9683fb1552
shengshi <eijiyey@163.com>
2023-06-08 11:15:00 +0800

    commit

--- app/src/main/java/com/qwqer/adplatform/net/Urls.java
+++ app/src/main/java/com/qwqer/adplatform/net/Urls.java
@@ -4,7 +4,7 @@ public interface Urls {
 
     String deleteAddress = "customer/address-book/delete/{id}"; //删除地址
     //v2.0.3-用户|商户获取广告信息
-    String advertInfo_user = "/express-inland-app/user/advertInfo/app/detail-customer";
+    String advertInfo_user = "/rests/advertInfo/detail-customer";
     /**
      * v2.0.3-用户|商户获取开屏广告信息
      * 经邓优化，统一用上面的接口
@@ -13,7 +13,7 @@ public interface Urls {
 
 
     //v2.0.3-配送员获取广告信息
-    String advertInfo_driver = "/express-inland-app/user/advertInfo/app/detail-worker";
+    String advertInfo_driver = "/rests/advertInfo/detail-worker";
     /**
      * v2.0.3-配送员获取开屏广告信息
      * 经邓优化，统一用上面的接口
